3f82786b59741bc4e44facc03fff5c26f9afa82e
Dody Gunawinata <dodyg@silverkeytech.com>
2012-06-16 14:16:36 +0200

    Add simple jumps example
    It's basic. It needs more complex and thorough examples.

--- src/Jumps.kt
+++ src/Jumps.kt
@@ -2,10 +2,14 @@ package Kotlin101.Jumps
 
 fun main (args : Array<String>){
 
-    @loop for (i in 1.50){
-        print($i)
-        if (i == 25){
+    @printAgain println("print again")
 
+    @loop for (i in 1..50){
+        print("$i,")
+        if (i == 25){
+            break @loop
         }
     }
+    println("")
+    println("end of line")
 }
