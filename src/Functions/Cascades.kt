4650c0da62324b958162ea9ab58df41f2b8f27e5
Dody Gunawinata <dodyg@silverkeytech.com>
2012-07-28 16:55:14 +0200

    Add sub object creation in with example

--- src/Functions/Cascades.kt
+++ src/Functions/Cascades.kt
@@ -1,11 +1,14 @@
 package Kotlin101.Functions.Cascades
 
 fun main (Args : Array<String>){
-    /* This call utilizes extension function and infix call */
+    /* This call utilizes extension function and infix call. It is handy to deal with pesky Java object initializations */
     var superman = Superman() with {
         name = "Lux Luthor"
         punch()
         kick()
+        sidekick = Spiderman() with {
+            special()
+        }
     }
 }
 
@@ -16,8 +19,23 @@ public fun <T>T.with(operations : T.() -> Unit) : T {
 
 public class Superman() {
     var name : String = "Clark Kent"
+    var sidekick : Sidekick = Robin()
 
     public fun punch() : Unit = println("$name punches")
     public fun fly() : Unit = println("$name flies")
     public fun kick() : Unit = println("$name kicks")
+}
+
+trait Sidekick {
+    public abstract fun special()
+}
+
+public class Spiderman() : Sidekick {
+    var name : String = "Peter Parker"
+    override fun special() = println("$name webs")
+}
+
+public class Robin() : Sidekick {
+    var name : String = "Robin"
+    override fun special() = println("$name is useless")
 }
