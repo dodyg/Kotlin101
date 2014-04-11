4e02f6520796865d340eae95660a49c5f3084cee
Dody Gunawinata <dodyg@silverkeytech.com>
2014-04-12 00:01:44 +0200

    Add Inline function example

---
+++ src/Functions/Inline.kt
@@ -0,0 +1,13 @@
+package  Kotlin101.Functions.Inline
+
+
+[inline]
+fun sayHello(number : Int){
+    println("hello world $number")
+}
+
+public fun main(args : Array<String>){
+    (1..10).forEach { i ->
+        sayHello(i)
+    }
+}
